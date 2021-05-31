3bd87bc1cb13e387d7939f77e2e832ab52299d01
Alfred A <20140832+carrotsnotfound@users.noreply.github.com>
2021-05-31 10:44:52 +0800

    first commit for packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='gf_python',
+      version='0.1',
+      description='Utilities to work with GF trees from Python',
+      url='http://github.com/smucclaw/gf-python',
+      author='Inari Listenmaa',
+      author_email='',
+      license='',
+      packages=['gf_python'],
+      install_requires=['pgf'],
+      zip_safe=False)
